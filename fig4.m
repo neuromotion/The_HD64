dd0d8e6358980d40006466ce0d015a0c97f91483
Sam Parker <samuel.parker5@gmail.com>
2024-09-27 10:33:02 -0400

    Updated fig 4 code

--- fig4.m
+++ fig4.m
@@ -1,108 +1,65 @@
-%% fig4.m - The code to plot figure 4 of "An active electronic bidirectional interface for high resolution interrogation of the spinal cord"
+%% fig4.m - The code to plot figure 4 of "An active electronic, high-density epidural paddle array for chronic spinal cord neuromodulation"
 % Author: Samuel Parker
-% Last Modified: 21-May-2024
+% Last Modified: 27-Sep-2024
 
 clear all; close all; clc
+%% Panel C
 
-%% Panels A - I
-
-load("fig4_panelsA_I.mat")
-
-cathodes = [3, 3, 3];
-anodes = [-1, 4, 14];
-elec_configs = ["C3", "C3A4", "C3A14"];
-config_names = ["Mono", "Narrow Bipolar", "Wide Bipolar"];
-musc_names = ["RightEDL", "RightBF", "RightGas", "LeftEDL", "LeftBF", "LeftGas"];
-colors = ["#06D6A0", "#B04CB9", "#073B4C", "#EF476F", "#FFD166", "#FD833D"];
-
-% Plot HD64 maps, save them as SVGs
-for i = 1:length(elec_configs)
-    fid = fopen(strcat(config_names(i), "HD64 Map.svg"), "w+");
-    out = plotHD64(cathodes(i), anodes(i));
-    fprintf(fid, "%s", out);
-    fclose(fid);
-end
+load("fig4_panelC.mat")
+colors = hex2rgb(["#bff4ff", "#80e9ff", "00d3ff"]);
 
 figure()
-% Plot mono vs narrow rAUC and Selectivity Index
-tcl = tiledlayout(1, 2);
-nexttile
-for i = 1:length(musc_names)
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_mean_rAUC")), fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_std_rAUC")), "--", "Color", colors(i)); hold on
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_mean_rAUC")), fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_std_rAUC")), "-", "Color", colors(i)); hold on
-end
-legend_str = repmat("", 2*length(musc_names), 1); legend_str(2:2:end) = musc_names;
-legend(legend_str, 'Location', "southoutside", "Orientation", "horizontal", "NumColumns", 2);
-xticks(500:500:1500)
-xlim([450, 1550])
-xlabel("EES amp (uA)")
-ylabel("rAUC (a.u.)")
+tcl = tiledlayout(4, 1);
+trial_stats = grpstats(fig4_panelC.data, ["Distance_mm", "EES_Amp_mA"], ["mean", "std"]);
+unique_bipoles = unique(trial_stats.Distance_mm);
+for i = length(unique_bipoles):-1:1
+    this_bipole_data = trial_stats(trial_stats.Distance_mm == unique_bipoles(i), :);
+    nexttile
+    unique_amps = unique(this_bipole_data.EES_Amp_mA);
+    for ampIdx = 1:length(unique_amps)    
+        upper_bound = this_bipole_data{this_bipole_data.EES_Amp_mA == unique_amps(ampIdx), 6:2:end} + this_bipole_data{this_bipole_data.EES_Amp_mA == unique_amps(ampIdx), 7:2:end};
+        lower_bound = this_bipole_data{this_bipole_data.EES_Amp_mA == unique_amps(ampIdx), 6:2:end} - this_bipole_data{this_bipole_data.EES_Amp_mA == unique_amps(ampIdx), 7:2:end};
+        time2 = [fig4_panelC.time_us, fliplr(fig4_panelC.time_us)];
+        shadedArea = [upper_bound, fliplr(lower_bound)];
+        fill(time2, shadedArea, colors(ampIdx, :), "FaceAlpha", 0.5); hold on
+        plot(fig4_panelC.time_us, this_bipole_data{this_bipole_data.EES_Amp_mA == unique_amps(ampIdx), 6:2:end}, "Color", "#000000");
+    end
+    title(sprintf("Bipole Center Distance: %0.2fmm", unique_bipoles(i)));
+    xlim([0, 500])
+    ylim([-0.225, 0.225])
+    xticks(0:100:500)
+    xlabel("Time (us)")
+    ylabel("LFP (mV)")
 
-nexttile
-for i = 1:length(musc_names)
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_mean_SelIdx")), fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_std_SelIdx")), "--", "Color", colors(i)); hold on
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_mean_SelIdx")), fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_std_SelIdx")), "-", "Color", colors(i)); hold on
 end
-legend_str = repmat("", 2*length(musc_names), 1); legend_str(2:2:end) = musc_names;
-legend(legend_str, 'Location', "southoutside", "Orientation", "horizontal", "NumColumns", 2);
-xticks(500:500:1500)
-xlim([450, 1550])
-xlabel("EES amp (uA)")
-ylabel("Selectivity Index (a.u.)")
-title(tcl, "Monopolar (dashed) vs Narrow Bipolar (solid)")
 
+%% Panel D
+
+load("fig4_panelD.mat")
+
+unique_amps = unique(fig4_panelD.eesAmp_mA);
 figure()
-% Plot mono vs wide rAUC and Selectivity Index
-tcl = tiledlayout(1, 2);
-nexttile
-for i = 1:length(musc_names)
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_mean_rAUC")), fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_std_rAUC")), "--", "Color", colors(i)); hold on
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_mean_rAUC")), fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_std_rAUC")), "-", "Color", colors(i)); hold on
-end
-legend_str = repmat("", 2*length(musc_names), 1); legend_str(2:2:end) = musc_names;
-legend(legend_str, 'Location', "southoutside", "Orientation", "horizontal", "NumColumns", 2);
-xticks(500:500:1500)
-xlim([450, 1550])
-xlabel("EES amp (uA)")
-ylabel("rAUC (a.u.)")
+tcl = tiledlayout(3, 1);
+for ampIdx = 1:length(unique_amps)
+    this_amp = unique_amps(ampIdx);
 
-nexttile
-for i = 1:length(musc_names)
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_mean_SelIdx")), fig4_panelsA_I.(strcat(elec_configs(1), "_", musc_names(i), "_std_SelIdx")), "--", "Color", colors(i)); hold on
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_mean_SelIdx")), fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_std_SelIdx")), "-", "Color", colors(i)); hold on
+    nexttile
+    gscatter(fig4_panelD.onsetTime_us(fig4_panelD.eesAmp_mA == this_amp), fig4_panelD.distance_mm(fig4_panelD.eesAmp_mA == this_amp), fig4_panelD.distance_mm(fig4_panelD.eesAmp_mA == this_amp));
+    xlabel("Peak Onset Time [us]")
+    ylabel("Distance from Stimulation Bipole [mm]");
+    title(sprintf("%d mA", this_amp));
 end
-legend_str = repmat("", 2*length(musc_names), 1); legend_str(2:2:end) = musc_names;
-legend(legend_str, 'Location', "southoutside", "Orientation", "horizontal", "NumColumns", 2);
-xticks(500:500:1500)
-xlim([450, 1550])
-xlabel("EES amp (uA)")
-ylabel("Selectivity Index (a.u.)")
-title(tcl, "Monopolar (dashed) vs Wide Bipolar (solid)")
-
-figure()
-% Plot narrow vs wide rAUC and Selectivity Index
-tcl = tiledlayout(1, 2);
-nexttile
-for i = 1:length(musc_names)
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_mean_rAUC")), fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_std_rAUC")), "--", "Color", colors(i)); hold on
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_mean_rAUC")), fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_std_rAUC")), "-", "Color", colors(i)); hold on
+%% functions
+function outColor = hex2rgb(string_vector)
+    outColor = nan(length(string_vector), 3);
+    for i = 1:length(string_vector)
+        hex = char(string_vector(i));
+        if hex(1) == "#"
+            hex(1) = [];
+        end
+        r = hex2dec(hex(1:2)) ./255;
+        g = hex2dec(hex(3:4)) ./255;
+        b = hex2dec(hex(5:6)) ./255;
+        outColor(i, :) = [r, g, b];
+    end
 end
-legend_str = repmat("", 2*length(musc_names), 1); legend_str(2:2:end) = musc_names;
-legend(legend_str, 'Location', "southoutside", "Orientation", "horizontal", "NumColumns", 2);
-xticks(500:500:1500)
-xlim([450, 1550])
-xlabel("EES amp (uA)")
-ylabel("rAUC (a.u.)")
-
-nexttile
-for i = 1:length(musc_names)
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_mean_SelIdx")), fig4_panelsA_I.(strcat(elec_configs(3), "_", musc_names(i), "_std_SelIdx")), "--", "Color", colors(i)); hold on
-    errorbar(fig4_panelsA_I.stimAmp_uA, fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_mean_SelIdx")), fig4_panelsA_I.(strcat(elec_configs(2), "_", musc_names(i), "_std_SelIdx")), "-", "Color", colors(i)); hold on
-end
-legend_str = repmat("", 2*length(musc_names), 1); legend_str(2:2:end) = musc_names;
-legend(legend_str, 'Location', "southoutside", "Orientation", "horizontal", "NumColumns", 2);
-xticks(500:500:1500)
-xlim([450, 1550])
-xlabel("EES amp (uA)")
-ylabel("Selectivity Index (a.u.)")
-title(tcl, "Wide Bipolar (dashed) vs Narrow Bipolar (solid)")
